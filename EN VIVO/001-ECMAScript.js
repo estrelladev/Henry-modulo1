22190928fc9c49f5cff34c886daa6a762bd642c9
estrelladev <gujo716@gmail.com>
2024-07-22 10:10:54 -0600

    first 1 live class

---
+++ EN VIVO/001-ECMAScript.js
@@ -0,0 +1,10 @@
+//Objetivos
+//Conocer que es Ecma
+//Identificar new features
+//Demostrar como se usa en el dia a dia
+//Usar Chatgpt chido
+
+//PROYECTO
+//Descargar el proyecto e instalar las dependencias --- npm install
+//Familiarizarse con la estructura
+//Vincular
